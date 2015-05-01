ba50b924837ddf1cc9c150ec6238d53dd37b2127
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-01 11:53:12 -0400

    port index and css from repo

---
+++ app/controllers/grocery_controller.rb
@@ -0,0 +1,6 @@
+class GroceryController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'grocery' => "grocery#index"
+  root 'grocery#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
